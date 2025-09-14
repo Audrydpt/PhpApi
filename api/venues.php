41a07e62087dfd040bcd178f7e182f04f2dae5d9
audry <audry.dupont@condorcet.be>
2025-09-14 13:22:18 +0200

    test3

--- api/venues.php
+++ api/venues.php
@@ -21,7 +21,7 @@ try {
     $province = $_GET['province'] ?? null;
 
     if (!$province) {
-        throw new Exception("Paramètre 'province' requis (H, Lx, L, BBW, N)");
+        throw new Exception("Paramètre 'province' requis (H, Lx, L, BBW, N) test");
     }
 
     $validProvinces = ['H', 'Lx', 'L', 'BBW', 'N'];
